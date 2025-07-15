a99818ecd147cc86a24b2ba7ad370b4078cf225e
Iaienk <la-tua-email@example.com>
2025-07-15 10:19:16 +0200

    tests config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'sass'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Per usare API globali come describe, it, expect
+    environment: 'jsdom', // Ambiente per i test DOM
+    setupFiles: './src/tests/setup.js', // File per setup globale (es. import jest-dom)
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
 })
