214c8ffb9e699ee7199b34722ed1e708fbdcda32
Iaienk <la-tua-email@example.com>
2025-07-19 23:24:50 +0200

    adeguamento di deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,19 +3,17 @@ set -e
 
 cd /usr/local/repos/blog-fe-mt || exit
 
-# Branch di deploy (default main)
 BRANCH="${1:-main}"
 
-# Sincronizza forzatamente con il remoto
+# 1) Sincronizza con il remoto, scartando ogni modifica
 git fetch origin
 git reset --hard origin/"$BRANCH"
-git clean -fd
 
-# 1) Rimuovi solo la vecchia dist, NON il .tar.gz
+# 2) Rimuovi la vecchia dist
 rm -rf dist
 
-# 2) Estrai l’artifact compresso (che contiene già dist/)
+# 3) Estrai l’artifact compresso (package.tar.gz dev’essere ancora presente)
 tar -xzf package.tar.gz
 
-# 3) Pulisci l’archivio
+# 4) Pulisci l’archivio
 rm package.tar.gz
