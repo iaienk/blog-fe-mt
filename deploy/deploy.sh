7fd86251ab3f5b38250d776c1150231e1a60c9a7
Iaienk <la-tua-email@example.com>
2025-07-19 23:06:52 +0200

    deploy adjustment2

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
+# Cartella del progetto sul server
 cd /usr/local/repos/blog-fe-mt || exit
 
-# se non passo argomenti, uso 'main'
+# Branch di deploy (default a "main" se non lo passi)
 BRANCH="${1:-main}"
 
-git fetch
-git checkout "$BRANCH"
-git pull
+# 1) Sincronizza forzatamente con il remoto, scartando ogni modifica locale
+git fetch origin
+git reset --hard origin/"$BRANCH"
+git clean -fd
 
-rm -rf dist
-tar -xzvf package.tar.gz
+# 2) Pulisci eventuali build precedenti
+rm -rf dist package.tar.gz
+
+# 3) Estrai l’artifact (assicurati che il tuo workflow scp abbia copiato package.tar.gz qui)
+tar -xzf package.tar.gz
+
+# 4) Rimuovi l’archivio
 rm package.tar.gz
