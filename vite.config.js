e719c8310b6cf7ebdca95b539ac4c605138e88ce
Iaienk <la-tua-email@example.com>
2025-07-20 00:16:51 +0200

    plugin for assets

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sass from 'sass'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/assets/*', dest: 'assets' }
+      ]
+    })
+  ],
   test: {
     globals: true, // Per usare API globali come describe, it, expect
     environment: 'jsdom', // Ambiente per i test DOM
